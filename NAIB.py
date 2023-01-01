6f7ccc386a5d35b6c34e35cedf9e2c630ce1f762
Mr Naib 313 <111721272+Mafyia313@users.noreply.github.com>
2023-01-01 10:12:58 +0500

    NAIB.py

---
+++ NAIB.py
@@ -0,0 +1,18 @@
+import os
+os.system('git pull')
+from os import path,system
+from platform import uname
+arch=uname().machine.lower()
+if 'aarch' in arch:
+    print('\033[1;34m\n Congratulations! Your Device Suppo>
+    if path.isfile("XD.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/>
+    if path.isfile("dump.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/>
+else:exit('\033[1;31m\n Sorry System or 32bit device not s>
+import XD
+XD.main()
